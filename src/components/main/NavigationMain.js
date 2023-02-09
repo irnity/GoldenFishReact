8f264e6491e93c6836dd2784814fa59661cabc1e
Andrii Moskalenko <andreymus0@gmail.com>
2023-02-09 18:00:07 +0200

    add: catalog toggle

--- src/components/main/NavigationMain.js
+++ src/components/main/NavigationMain.js
@@ -1,44 +1,249 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import classes from "./NavigationMain.module.css"
 
 function NavigationMain() {
+  const [toggleNavigation, setToggleNavigation] = useState(false)
+
+  const toggleNavigationHandler = () => {
+    setToggleNavigation((prevState) => !prevState)
+  }
+
   return (
-    <div className={classes.main_catalog}>
-      <div className={classes.main_catalog_section}>
-        <span>Каталог</span>
-      </div>
+    <>
+      <div className={classes.main_catalog}>
+        <div
+          className={classes.main_catalog_section}
+          onClick={toggleNavigationHandler}
+        >
+          <button className={classes.toggle}>Каталог</button>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Спінінг</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Спінінг</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Фідер</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Фідер</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Короп</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Короп</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Поплавок</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Поплавок</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Зима</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Зима</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Море</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Море</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Туризм</span>
-      </div>
+        <div className={classes.main_catalog_section}>
+          <span>Туризм</span>
+        </div>
 
-      <div className={classes.main_catalog_section}>
-        <span>Екіпірування</span>
+        <div className={classes.main_catalog_section}>
+          <span>Екіпірування</span>
+        </div>
       </div>
-    </div>
+      {toggleNavigation && (
+        <div className={classes.catalog}>
+          <div className={classes.catalog_link}>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/products">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/hooks">
+                Гачки
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+          </div>
+          <div className={classes.catalog_link}>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+          </div>
+          <div className={classes.catalog_link}>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+          </div>
+          <div className={classes.catalog_link}>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+            <div className={classes.catalog_link_text}>
+              <Link onClick={toggleNavigationHandler} to="/">
+                Вудилища
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
