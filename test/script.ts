60228df1d600d5af51bb880cd0f1b044e8156d42
AndriiMoskalenko <andreymus0@gmail.com>
2023-03-03 12:07:59 +0200

    update: package add TypeScript

---
+++ test/script.ts
@@ -0,0 +1,20 @@
+// Primitives : number, string, boolean
+// More complex types: arrays, objects
+// function types, parameters
+
+// primitives
+
+let age: number
+
+age = 12.1
+
+let userName: string
+
+userName = "Andrii"
+
+let isTeacher: boolean
+
+isTeacher = true
+
+// More complex types: arrays, objects
+
