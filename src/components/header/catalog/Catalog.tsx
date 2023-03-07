9a024e56996ba559abfa2b232e37dc1d7b9690b6
AndriiMoskalenko <andreymus0@gmail.com>
2023-03-07 13:32:23 +0200

    update: animation with: {Transition}

--- src/components/header/catalog/Catalog.tsx
+++ src/components/header/catalog/Catalog.tsx
@@ -2,35 +2,37 @@ import { useState, FunctionComponent } from "react"
 import { Link } from "react-router-dom"
 import CatalogList from "./CatalogList"
 import classes from "./Catalog.module.css"
+import { Transition } from "react-transition-group"
+
 import { catalogList as information } from "./Info"
 
 interface CatalogProps {}
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+}
+
 const Catalog: FunctionComponent<CatalogProps> = () => {
   const [toggleNavigation, setToggleNavigation] = useState(false)
-  const [flade, setFlade] = useState(classes.catalog_in)
   const [list, setList] = useState<string[]>([])
 
   const toggleNavigationHandler = (data: string[]) => {
     // check if list has data or its new data
-    // then update list
+    // if new then update list
     if (JSON.stringify(list) !== JSON.stringify(data)) {
       setToggleNavigation((prevState) => (prevState = true))
-      setFlade(classes.catalog_in)
-      setList((prevState) => (prevState = data))
+      setList(data)
       return
-      // if new data = old data then catalog will be closed
+      // if new data === old data then catalog will be closed
     } else {
       fadeOut()
     }
   }
 
   function fadeOut() {
-    setFlade(classes.catalog_out)
-    setTimeout(() => {
-      setToggleNavigation((prevState) => (prevState = false))
-      setList((prevState) => (prevState = []))
-    }, 0.3 * 1000)
+    setToggleNavigation((prevState) => (prevState = false))
+    setList((prevState) => (prevState = []))
   }
 
   return (
@@ -47,26 +49,60 @@ const Catalog: FunctionComponent<CatalogProps> = () => {
         ))}
       </div>
       {/* list one of section  */}
-      {toggleNavigation && (
-        <div className={flade}>
-          <div className={classes.catalog_link}>
-            {/* need keys for catalog */}
-            {list.map((catalog, index) => (
-              <div className={classes.catalog_link_text} key={index}>
-                <Link
-                  to={`/${catalog}`}
-                  className={classes.link}
-                  onClick={fadeOut}
-                >
-                  {catalog}
-                </Link>
+      <Transition
+        in={toggleNavigation}
+        timeout={animationTiming}
+        mountOnEnter
+        unmountOnExit
+        onEnter={() => console.log("onEnter")}
+        onEntering={() => console.log("onEntering")}
+        onEntered={() => console.log("onEntered")}
+        onExit={() => console.log("onExit")}
+        onExiting={() => console.log("onExiting")}
+        onExited={() => console.log("onExited")}
+      >
+        {(state) => {
+          const cssClasses = [
+            classes.catalog_block,
+            state === "entering"
+              ? classes.catalog_in
+              : state === "exiting"
+              ? classes.catalog_out
+              : null,
+          ]
+          return (
+            <div className={cssClasses.join(" ")}>
+              <div className={classes.catalog_link}>
+                {/* need keys for catalog */}
+                {list.map((catalog, index) => (
+                  <div className={classes.catalog_link_text} key={index}>
+                    <Link
+                      to={`/${catalog}`}
+                      className={classes.link}
+                      onClick={fadeOut}
+                    >
+                      {catalog}
+                    </Link>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
-      )}
+            </div>
+          )
+        }}
+      </Transition>
     </>
   )
 }
 
 export default Catalog
+// style={{
+//   position: "absolute",
+//   top: "30%",
+//   display: "flex",
+//   justifyContent: "space-around",
+//   width: "90%",
+//   height: "65%",
+//   boxShadow: "0px 0px 10px 0px black",
+//   backgroundColor: " white",
+
+// }
